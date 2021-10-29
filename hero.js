c3a2edf1e106a2c7b0aa217eacb4f13d31b1fdfc
lennartchua59 <82433697+lennartchua59@users.noreply.github.com>
2021-10-29 13:37:06 +0800

    test

---
+++ hero.js
@@ -0,0 +1,9 @@
+const superherom = { //-- using curly brace. it's an object
+    // define a function called fly,
+    fly :() => {
+        console.log("flying...")
+    }
+
+}
+
+module.export = superhero 
--- week2-ch2.js
+++ week2-ch2.js
@@ -2,3 +2,8 @@ let f = require("./formula")
 
 console.log("mulitply 5x 3 = " + f.mul(5, 3))
 console.log("add 4x 6 = " + f.add(4, 6))
+
+// -- 2nd demo using object as exports
+let h = require("./hero")
+// use function in the object
+h.fly()
