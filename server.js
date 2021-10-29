1b7829631b83ad3a89b3d1b035e9b698a7d62e67
lennartchua59 <82433697+lennartchua59@users.noreply.github.com>
2021-10-29 15:45:56 +0800

    save

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+// import modules
 const http = require("http")
-const hostname = "localhost"
+const fs = require("fs")
 const port = 3000
 const server = http.createServer(
     // listener function
@@ -9,7 +10,33 @@ const server = http.createServer(
         res.statusCode = 200
         res.setHeaderCode("Content-Type", "text/html")
         res.end("<html><body>welcome t dit 1b06 nodejs demo</body></html>")
+
+        // -- check request and respond accordingly
+        if(req.method =="GET"){
+            var fileURL = req.url
+
+            if(req.url =="/") // default file is index.html
+                fileURL = "/index.html"
+            
+            var filePath = path.resolve("./public" +fileURL)
+
+            // if file exist?
+            fs.exist(filePath, (exist)=>{
+                if(!exist){
+                    // show error page
+                    fileURL = "/error.html"
+                    filePath = path.resolve("./public" + fileURL)
+
+                }else {
+                    // file exist, respond with file
+                    res.setHeaderCode = 200
+                    res.setHeaderCode("Content-Type", "text/html")
+                }
+                fs.createReadStream(filePath).pipe(res)
+            })
+        }
     }
+
 )
 
 //-- make this server listen
